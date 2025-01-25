af32eafa542fc8fbb7aac71e7f74859a6d5ba88b
lucasmarqueshzx <lucas.marques@gauge.haus>
2025-01-25 14:19:54 -0300

    Subindo frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserListPageComponent } from './pages/user-list-page/user-list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: UserListPageComponent },
+];
---
+++ src/app/pages/user-list-page/user-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-list-page.component.html',
+  styleUrl: './user-list-page.component.css'
+})
+export class UserListPageComponent {
+
+}
