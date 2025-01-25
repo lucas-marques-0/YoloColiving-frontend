6c36f3aa8cb8c58fb3eed34dd1ae9aa7606c0ed0
Lucas Marques <lucas.marques@gauge.haus>
2025-01-25 15:10:30 -0300

    Atualizando angular CLI novamente

--- src/app/pages/user-list-page/user-list-page.component.ts
+++ src/app/pages/user-list-page/user-list-page.component.ts
@@ -4,14 +4,14 @@ import { UserListPageService } from './user-list-page.service';
 import { User } from '../../models/user.interface'
 
 @Component({
-  selector: 'app-user-list-page',
-  standalone: true,
-  imports: [
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  templateUrl: './user-list-page.component.html',
-  styleUrl: './user-list-page.component.css',
+    selector: 'app-user-list-page',
+    standalone: true,
+    imports: [
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    templateUrl: './user-list-page.component.html',
+    styleUrl: './user-list-page.component.css'
 })
 export class UserListPageComponent implements OnInit {
   users: User[] = [];
